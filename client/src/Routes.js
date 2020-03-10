8735d5fcb3c6cf0f98695d2688f2457b32a04ccb
Tim Vo <timtvo714@gmail.com>
2020-03-09 19:58:49 -0700

    added PrivateRouteWithLayout

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Redirect} from 'react-router-dom';
 import {RouteWithLayout} from './components';
+import {PrivateRouteWithLayout} from './components'
 import {Main as MainLayout, Minimal as MinimalLayout} from './layouts';
 import PrivateRoute from './components/routing/PrivateRoute';
 import {
