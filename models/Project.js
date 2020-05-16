15f2d577eea4305e740efecae1c4983d5b26c1fc
Tim Vo <timtvo714@gmail.com>
2020-05-16 12:07:00 -0700

    clean up part 2 backend

--- models/Project.js
+++ models/Project.js
@@ -91,6 +91,12 @@ const ProjectSchema = new Schema ({
       taskType: {
         type: String,
       },
+
+      // taskNumber: {
+      //   type: String,
+      //   uppercase: true,
+      //   default: shortid.generate,
+      // },
       subtasks: [
         {
           user: {
@@ -132,163 +138,6 @@ const ProjectSchema = new Schema ({
     },
   ],
 
-  stories: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-      },
-      storySummary: {
-        type: String,
-        required: true,
-      },
-
-      storyDescription: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-      },
-      avatar: {
-        type: String,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      },
-      isCompleted: {
-        type: Boolean,
-        default: false,
-      },
-      users: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-      },
-      storyPriority: {
-        type: Number,
-      },
-      subtasks: [
-        {
-          user: {
-            type: Schema.Types.ObjectId,
-            ref: 'users',
-          },
-          subtaskSummary: {
-            type: String,
-            required: true,
-          },
-
-          subtaskDescription: {
-            type: String,
-            required: true,
-          },
-          name: {
-            type: String,
-          },
-          avatar: {
-            type: String,
-          },
-          date: {
-            type: Date,
-            default: Date.now,
-          },
-          isCompleted: {
-            type: Boolean,
-            default: false,
-          },
-          users: {
-            type: Schema.Types.ObjectId,
-            ref: 'users',
-          },
-          taskPriority: {
-            type: Number,
-          },
-        },
-      ],
-    },
-  ],
-
-  tickets: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-      },
-      ticketNumber: {
-        type: String,
-        uppercase: true,
-        default: shortid.generate,
-      },
-      ticketSummary: {
-        type: String,
-        required: true,
-      },
-      ticketDescription: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-      },
-      avatar: {
-        type: String,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      },
-      isCompleted: {
-        type: Boolean,
-        default: false,
-      },
-      users: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-      },
-      ticketPriority: {
-        type: Number,
-      },
-      subtasks: [
-        {
-          user: {
-            type: Schema.Types.ObjectId,
-            ref: 'users',
-          },
-          subtaskSummary: {
-            type: String,
-            required: true,
-          },
-
-          subtaskDescription: {
-            type: String,
-            required: true,
-          },
-          name: {
-            type: String,
-          },
-          avatar: {
-            type: String,
-          },
-          date: {
-            type: Date,
-            default: Date.now,
-          },
-          isCompleted: {
-            type: Boolean,
-            default: false,
-          },
-          users: {
-            type: Schema.Types.ObjectId,
-            ref: 'users',
-          },
-          taskPriority: {
-            type: Number,
-          },
-        },
-      ],
-    },
-  ],
   date: {
     type: Date,
     default: Date.now,
